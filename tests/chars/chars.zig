6d5c0a09c75e75ff91344bb38d0b661f914eb374
koba <artem.kabatov22@gmail.com>
2026-09-21 20:13:51 +0300

    feat: match character literals

---
+++ tests/chars/chars.zig
@@ -0,0 +1,16 @@
+const plain = 'a';
+const digit = '7';
+const space = ' ';
+const nl = '\n';
+const cr = '\r';
+const tab = '\t';
+const apos = '\'';
+const backslash = '\\';
+const quote = '"';
+const hex = '\x41';
+const unicode = '\u{1F4A9}';
+const utf8_two_byte = 'é';
+const utf8_four_byte = '💩';
+const not_a_char = 'ab';
+const in_string = "it's fine";
+// don't touch apostrophes in comments
