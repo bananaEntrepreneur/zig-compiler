d3a169ce6d06acf3bc8e3bd35d68aafd7fa039e0
koba <artem.kabatov22@gmail.com>
2026-09-21 22:01:06 +0300

    feat: report unterminated string and character literals

--- tests/errors/errors.zig
+++ tests/errors/errors.zig
@@ -2,3 +2,13 @@ const a = 1;
 const b = #;
 const c = $x;
 const d = `2`;
+const ok_str = "fine";
+const ok_char = 'a';
+const ok_quoted = @"good";
+const bad_str = "unterminated
+const bad_char = 'ab';
+const no_close = 'x;
+const bad_quoted = @"unclosed
+// don't touch apostrophes in comments
+const in_str = "it's fine";
+const at_eof = "runs into EOF
